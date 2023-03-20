12aee7609465ff3f29f3a3b5e748fdda3ac3b517
Chris Vargas <gqcodes@gmail.com>
2023-03-20 17:25:36 -0400

    file clean-up

---
+++ .next/static/gSqH4w0YY8p6gJLnxkzXb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/431-e8aa107635774ba7.js","static/css/e4c4aa6865b6e71d.css","static/chunks/pages/index-00868783f7b56a91.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
